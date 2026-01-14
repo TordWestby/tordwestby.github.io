7cd3769f249f62de2c25e473059b3e7e4aa3a411
Tord Westby <tordhw@gmail.com>
2026-01-14 22:38:09 +0100

    React Project start
    Start of RyddigPC

---
+++ RyddigPc/src/html/components/App.jsx
@@ -0,0 +1,17 @@
+import '../../style/App.css'
+import Navbar from './Navbar'
+
+function App() {
+  return (
+    <>
+    <header>
+      <Navbar />
+    </header>
+    <div className='main-content'>
+      <h1>Hello World</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ RyddigPc/src/html/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import '../../style/navbar.css'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="">Placeholder</a></li>
+                <li><a href="">Placeholder</a></li>
+                <li><a href="">Placeholder</a></li>
+                <li><a href="">Placeholder</a></li>
+                <li><a href="">Placeholder</a></li>
+                <li><a href="">Placeholder</a></li>
+            </ul>
+        </nav>
+    );
+}
